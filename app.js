aa8489efa071175d6dff93d1c8108fc987366a74
Andres <Sebastianonza2007@gmail.com>
2026-07-23 16:23:23 -0500

    Use of ES in the code

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (_,  res) => {
-    res.send('Hola, estamos aprendiendo expreess con la ficha 3407184')
+// Creas y exportas la constante en el mismo archivo
+export const saludo = 'Hola, estamos aprendiendo express con la ficha 3407184';
+
+app.get("/", (_, res) => {
+    res.send(saludo); // La utilizas aquí directamente
 });
+
 app.listen(port, () => {
-    console.log('Servidor en funcionamiento en el puerto: ')
+    console.log(`Servidor en funcionamiento en el puerto: ${port}`);
 });
