a91d090ee165daf6cadf898ddfd57d213e0f548a
Andres <Sebastianonza2007@gmail.com>
2026-07-23 16:29:02 -0500

    correction in the package.json

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
-// Creas y exportas la constante en el mismo archivo
 export const saludo = 'Hola, estamos aprendiendo express con la ficha 3407184';
 
 app.get("/", (_, res) => {
-    res.send(saludo); // La utilizas aquí directamente
+    res.send(saludo);
 });
 
 app.listen(port, () => {
