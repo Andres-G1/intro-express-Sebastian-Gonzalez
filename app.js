21f227d957151afb39932acb15f71dd44ad33004
kamila <kailahurtado31@gmail.com>
2026-07-30 13:36:51 -0500

    list products

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 import express from 'express';
 import 'dotenv/config'
+import bodyParser from "body-parser" /*importacion modulo*/
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// config body-parser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 export const saludo = 'Hola, estamos aprendiendo express con la ficha 3407184';
 
 app.get("/", (_, res) => {
     res.send(saludo);
 });
+
+app.get("/products", (req, res) =>{
+    res.send(`<h1>Products list</h1>
+        <ol>
+        <li>Tv</li>
+        <li>Phone</li>
+        <li>printer</li>
+        </ol>`
+    )
+})
+
 app.listen(port, () => {
     console.log(`Servidor en funcionamiento en el puerto: ${port}`);
 });
